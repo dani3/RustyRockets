547be9dbd36c298b70b6476b437575464491508d
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-08-29 21:47:06 +0200

    Added SDL main loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
+const SCREEN_WIDTH: isize  = 1200;
+const SCREEN_HEIGHT: isize = 800;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("Starfield", SCREEN_WIDTH as u32, SCREEN_HEIGHT as u32)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    canvas.clear();
+    canvas.present();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+
+        canvas.present();
+    }
 }
