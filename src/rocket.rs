db1d6d0d9efb53cfa5adc9504ab0065c41ebe7aa
DMA <dma@logossmartcard.com>
2019-09-02 09:41:04 +0200

    Resolved lifetime issue

--- src/rocket.rs
+++ src/rocket.rs
@@ -12,7 +12,6 @@ const HEIGHT: u32 = 50;
 const WIDTH: u32 = 10;
 
 pub struct Rocket {
-    texture : Texture<'static>,
     texture_creator : TextureCreator<WindowContext>,
     position : Vector2D<f64>,
     velocity : Vector2D<f64>,
@@ -22,14 +21,11 @@ pub struct Rocket {
 impl Rocket {
     pub fn new(canvas: &Canvas<Window>, origin: Point) -> Self {
         let texture_creator: TextureCreator<_> = canvas.texture_creator();
-        let texture: Texture<'static> = texture_creator.create_texture_target(None, WIDTH, HEIGHT)
-                                     .expect("Failed to create a texture");
 
         let x = origin.x - HEIGHT as i32;
         let y = origin.y - WIDTH as i32 / 2;
 
         Rocket {
-            texture,
             texture_creator,
             position : Vector2D::new(x as f64, y as f64),
             velocity : Vector2D::new(0.0, 0.0),
@@ -52,12 +48,15 @@ impl Rocket {
     }
 
     pub fn show(&mut self, canvas: &mut Canvas<sdl2::video::Window>) {
-        let _ = canvas.with_texture_canvas(&mut self.texture, |texture_canvas| {
+        let mut texture = self.texture_creator.create_texture_target(None, WIDTH, HEIGHT)
+                                                       .expect("Failed to create a texture");
+
+        let _ = canvas.with_texture_canvas(&mut texture, |texture_canvas| {
             texture_canvas.set_draw_color(Color::RGBA(255, 0, 0, 255));
             texture_canvas.clear();
         });
 
         let _ = canvas.copy_ex(
-            &self.texture, None, Rect::new(100, 100, 50, 150), 0.0, Point::new(25, 150), false, false);
+            &texture, None, Rect::new(100, 100, 50, 150), 0.0, Point::new(25, 150), false, false);
     }
 }
