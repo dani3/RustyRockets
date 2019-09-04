b5314e2fa661e150368e7446919faa921ec2d6f4
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-09-04 18:55:54 +0200

    Rocket fitness calculated based on the distance

--- src/rocket.rs
+++ src/rocket.rs
@@ -1,14 +1,16 @@
+use crate::target::Target;
+use crate::dna::DNA;
+use crate::constants::LIFESPAN;
+
 use vector2d::Vector2D;
 
 use std::f64::consts::PI;
+use std::f64;
 
 use sdl2::render::{Canvas, TextureCreator};
 use sdl2::rect::{Rect, Point};
 use sdl2::video::{WindowContext, Window};
 use sdl2::pixels::Color;
-
-use crate::dna::DNA;
-use crate::constants::LIFESPAN;
 
 const HEIGHT: u32 = 25;
 const WIDTH: u32 = 5;
@@ -19,7 +21,8 @@ pub struct Rocket {
     velocity : Vector2D<f64>,
     acceleration : Vector2D<f64>,
     dna : DNA,
-    count : usize
+    count : usize,
+    fitness : f64
 }
 
 impl Rocket {
@@ -35,14 +38,17 @@ impl Rocket {
             velocity : Vector2D::new(0.0, 0.0),
             acceleration : Vector2D::new(0.0, 0.0),
             dna : DNA::new(LIFESPAN),
-            count : 0
+            count : 0,
+            fitness : 0.0
         }
     }
 
+    /// Applies some force
     fn apply_force(&mut self, force: Vector2D<f64>) {
         self.acceleration += force;
     }
 
+    /// Updates the position based on the acceleration and velocity.
     pub fn update(&mut self) {
         self.apply_force(self.dna.get_genes()[self.count]);
         self.count += 1;
@@ -56,6 +62,7 @@ impl Rocket {
         self.acceleration = Vector2D::new(0.0, 0.0);
     }
 
+    /// Draws the rocket
     pub fn show(&mut self, canvas: &mut Canvas<sdl2::video::Window>) {
         let mut texture = self.texture_creator.create_texture_target(None, WIDTH, HEIGHT)
                                               .expect("Failed to create a texture");
@@ -80,5 +87,25 @@ impl Rocket {
             , Point::new(WIDTH as i32 / 2, HEIGHT as i32 / 2)
             , false
             , false);
+    }
+
+    fn calulate_distance_to_target(&self, target : &Target) -> f64 {
+        let vx = self.position.x;
+        let vy = self.position.y;
+
+        let ux = target.get_position().x as f64;
+        let uy = target.get_position().y as f64;
+
+        ((ux - vx).powi(2) + (uy - vy).powi(2)).sqrt()
+    }
+
+    pub fn calculate_fitness(&mut self, target : &Target) {
+        let dist = self.calulate_distance_to_target(target);
+
+        if dist == 0.0 {
+            self.fitness = 1.0;
+        } else {
+            self.fitness = 1.0 / dist;
+        }
     }
 }
